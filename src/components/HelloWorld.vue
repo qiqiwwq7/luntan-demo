f9cb0973d50b796138ab10bf551f9cd72ede9d61
qiqiwwq7 <737242523@qq.com>
2017-11-26 22:47:35 +0800

    create demo

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+      <br>
+      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
+      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
+      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+    </ul>
+    <group>
+      <cell title="title" value="value"></cell>
+    </group>
+    <group :title="value[0] + ' ' + value[1] + ':' + value[2]">
+      <datetime-range :title="'Choose'" start-date="2017-01-01" end-date="2017-02-02" :format="'daterange-format'" v-model="value" @on-change="onChange"></datetime-range>
+    </group>
+  </div>
+</template>
+
+<script>
+import { Group, Cell, DatetimeRange } from 'vux'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      value: ['2017-01-15', '03', '05']
+    }
+  },
+  components: {
+    Group,
+    Cell,
+    DatetimeRange
+  },
+  methods: {
+    onChange (val) {
+      console.log('change', val)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less">
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
