26e7efb12c5ed5ec3f69a3862e4a41877071a53f
qiqiwwq7 <wangwenqi@973.sinanet.com>
2017-11-27 19:32:38 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,49 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <header class='luntan-header'>
+       <div class=''></div>
+    </header>
+    <div class="luntan">
+        <router-view/>
+    </div>
   </div>
 </template>
-
 <script>
+import { Icon } from 'vux'
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    Icon
+  }
 }
 </script>
+<style lang='less'>
+@import url('./assets/css/pink.less');
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+/* 公共 */
+.luntan-header {
+  background: #1A2946;
+}
+.luntan {
+  width: 100%;
+  height: 100%;
+}
+/*横屏  */
+@media all and (orientation: landscape) {
+  .luntan-header {
+    width: 60px;
+    height:100%;
+    float: left;
+  }
+}
+/*竖屏  */
+@media all and (orientation: portrait) {
+  .luntan-header {
+    width: 100%;
+    height:60px;
+    position: absolute;
+    left: 0;
+    bottom:0;
+  }
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,68 +1,55 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
+  <div>
+    <ul class="type">
+      <li>官方公告</li>
+      <li>最新发帖</li>
+      <li>最新回复</li>
     </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-    <group>
-      <cell title="title" value="value"></cell>
-    </group>
-    <group :title="value[0] + ' ' + value[1] + ':' + value[2]">
-      <datetime-range :title="'Choose'" start-date="2017-01-01" end-date="2017-02-02" :format="'daterange-format'" v-model="value" @on-change="onChange"></datetime-range>
-    </group>
   </div>
 </template>
-
 <script>
-import { Group, Cell, DatetimeRange } from 'vux'
+// import { Group, Cell, DatetimeRange } from 'vux'
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App',
-      value: ['2017-01-15', '03', '05']
+       // kk
     }
   },
   components: {
-    Group,
-    Cell,
-    DatetimeRange
+    // kk
   },
   methods: {
-    onChange (val) {
-      console.log('change', val)
-    }
+  // kk
   }
 }
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="less">
-h1, h2 {
-  font-weight: normal;
+<style lang="less" scoped>
+.type {
+  width: 100%;
+  height: 50px;
+  background: #1A2946;
+  li {
+    padding: 0 10px;
+    float: left;
+    background: #1A2946;
+    color: #fff;
+    line-height: 50px;
+    cursor: pointer;
+  }
+  li:hover {
+    color: #CE7E03;
+    border-bottom: 1px solid #CE7E03;
+    background: #081529;
+  }
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+/*横屏  */
+@media all and (orientation: landscape) {
+
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+/*竖屏  */
+@media all and (orientation: portrait) {
+
 }
 </style>
